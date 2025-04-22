01b7b968f0a7ca7ea2b657f1095d8481f2d8c3af
Loli_Wolf <2233284845@qq.com>
2025-04-23 00:24:26 +0800

    feat(generate): 为不同物种生成无环境无活性启动子序列
    - 添加物种参数，支持玉米、高粱和拟南芥
    - 根据物种选择相应的模型文件
    - 对于高粱，先创建模型实例再加载状态字典
    -优化代码结构，提高可读性和可维护性

--- Diff-Promoter/generate.py
+++ Diff-Promoter/generate.py
@@ -1,4 +1,4 @@
-# D:\code\git-project\Diff-Promoter\.venv\Scripts\python.exe D:\code\git-project\Diff-Promoter\Diff-Promoter\generate.py -task_id 1
+# D:\code\git-project\Diff-Promoter\.venv\Scripts\python.exe D:\code\git-project\Diff-Promoter\Diff-Promoter\generate.py -task_id 1 -species maize
 import argparse
 import sys
 import time
@@ -18,6 +18,7 @@ from itertools import islice
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-task_id', type=int, default=1, help='backend task id')
+parser.add_argument('-species', type=str, help='maize | sorghum | arabidopsis')
 args = parser.parse_args()
 
 device = None
@@ -36,16 +37,31 @@ if device is None:
     device = torch.device('cpu')
 print(f"Using device: {device}")
 
+species = args.species
+model_name = ''
+if species == 'maize':
+    model_name = 'maize'
+elif species == 'sorghum':
+    model_name = 'sorghum'
+elif species == 'arabidopsis':
+    model_name = 'arabidopsis'
+else:
+    raise ValueError("species参数错误")
+
 timesteps = 1000
 gaussian_diffusion = GaussianDiffusion(timesteps=timesteps)
 
 os.makedirs('res', exist_ok=True)
 # model = UNetModel()
-model = torch.load('params/model.pkl', map_location=device,  weights_only=False)
+model = torch.load('params/' + model_name + '.pkl', map_location=device, weights_only=False)
+if species == 'sorghum' or species == 'arabidopsis':
+    model = UNetModel()  # 先创建模型实例
+    state_dict = torch.load('params/' + model_name + '.pkl', map_location=device)  # 加载状态字典
+    model.load_state_dict(state_dict)
 model.to(device)
 model.eval()
-g_seqs = generate_gen(model, gaussian_diffusion, 1) # 输入 生成几条
-with open('Diff-Promoter/res/' + f"{args.task_id}" + '.fasta', 'w') as f: # 生成 fasta 文件
+g_seqs = generate_gen(model, gaussian_diffusion, 1)  # 输入 生成几条
+with open('Diff-Promoter/res/' + f"{args.task_id}" + '.fasta', 'w') as f:  # 生成 fasta 文件
     for idx, seq in enumerate(g_seqs):
         f.write('>gen_' + str(idx) + '\n')
         f.write(seq + '\n')
