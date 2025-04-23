97d4feb0363b7d08d89e0513411b478dbb0c31d5
Loli_Wolf <2233284845@qq.com>
2025-04-23 13:05:56 +0800

    feat(Predictor): 添加启动子预测功能
    - 新增 muta.py 和 scores.py 脚本，实现启动子预测和可视化- 更新 .gitignore 文件，添加新的结果目录
    - 优化设备选择逻辑，支持 CPU 和 GPU- 增加参数解析，支持不同物种和环境的选择
    - 修复文件路径和模型加载问题

--- Predictor/muta.py
+++ Predictor/muta.py
@@ -1,3 +1,7 @@
+# D:\code\git-project\Diff-Promoter\.venv\Scripts\python.exe D:\code\git-project\Diff-Promoter\Predictor\muta.py -task_id 1 -env NDM -species maize -gene gene_0:ACCTTGAAAGTATTTTTCACTGTATTTTGACGTCAGCCCATCACAATCTCGAAACCTTAAAGCTTATCGCGGCTTGCCCCGCCCACCACACGCACTGCCATGAATCCCCGCGCACTGATCATGCTCAGCACTGTCGTTTTCAGTGGGGGTGGCCAGAAAAGAGACCAGCT,gene_1:ACCTTGAAAGTATTTTTCACTGTATTTTGACGTCAGCCCATCACAATCTCGAAACCTTAAAGCTTATCGCGGCTTGCCCCGCCCACCACACGCACTGCCATGAATCCCCGCGCACTGATCATGCTCAGCACTGTCGTTTTCAGTGGGGGTGGCCAGAAAAGAGACCAGCT
+import argparse
+import os
+
 import torch
 from adv_model import Net
 import csv
@@ -7,11 +11,60 @@ import numpy as np
 from itertools import islice
 import random
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-task_id', type=int, help='后端task_id')
+parser.add_argument('-env', type=str, help='使用环境')
+parser.add_argument('-species', type=str, help='maize | sorghum | arabidopsis')
+parser.add_argument('-gene', type=str,required=True, help='启动子序列，格式为"基因名:序列"，多个启动子用逗号分隔')
 
-device = torch.device('cuda:4')
+args = parser.parse_args()
+task_id = args.task_id
+env = args.env
+
+device = None
+if hasattr(torch, 'cuda') and torch.cuda.is_available():
+    try:
+        device = torch.device('cuda:0')
+        # 简单测试CUDA是否真的可用
+        torch.zeros(1).to(device)
+    except:
+        device = None
+
+# if device is None and hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+#     device = torch.device('mps')
+
+if device is None:
+    device = torch.device('cpu')
+
+# 环境选择
+species = args.species
+file_path_species = ''
+if species == 'maize':
+    file_path_species = 'maize'
+elif species == 'sorghum':
+    file_path_species = 'sorghum'
+elif species == 'arabidopsis':
+    file_path_species = 'arabidopsis'
+else:
+    raise ValueError("species参数错误")
+file_path = f'Predictor-Guidance/{file_path_species}/'
+if env == 'WDM':
+    filename = file_path + "wdm/model_params.pkl"
+elif env == 'NDM':
+    filename = file_path + "ndm/model_params.pkl"
+elif env == 'NDT':
+    filename = file_path + "ndt/model_params.pkl"
+elif env == 'NLT':
+    filename = file_path + "nlt/model_params.pkl"
+elif env == 'WDT':
+    filename = file_path + "wdt/model_params.pkl"
+elif env == 'WLT':
+    filename = file_path + "wlt/model_params.pkl"
+else:
+    raise ValueError("env参数错误")
 
 model = Net()
-model.load_state_dict(torch.load('model_params.pkl', map_location=device)) # 选六种环境 18
+model.load_state_dict(torch.load(filename, map_location=device)) # 选六种环境 18
 model.to(device)
 model.eval()
 
@@ -22,7 +75,26 @@ def seq2tensor(seq, device=device):
         encode_seq.append(one_hot[element])
     return torch.tensor(encode_seq, dtype=torch.float, device=device).t().unsqueeze(dim=0)
 
-dict_promoter = {"gen_0":"ACCTTGAAAGTATTTTTCACTGTATTTTGACGTCAGCCCATCACAATCTCGAAACCTTAAAGCTTATCGCGGCTTGCCCCGCCCACCACACGCACTGCCATGAATCCCCGCGCACTGATCATGCTCAGCACTGTCGTTTTCAGTGGGGGTGGCCAGAAAAGAGACCAGCT"}
+# 解析启动子参数
+dict_promoter = {}
+if args.gene:
+    for item in args.gene.split(','):
+        gene, seq = item.split(':')
+        dict_promoter[gene] = seq.upper()
+
+# dict_promoter = {"gen_0":"ACCTTGAAAGTATTTTTCACTGTATTTTGACGTCAGCCCATCACAATCTCGAAACCTTAAAGCTTATCGCGGCTTGCCCCGCCCACCACACGCACTGCCATGAATCCCCGCGCACTGATCATGCTCAGCACTGTCGTTTTCAGTGGGGGTGGCCAGAAAAGAGACCAGCT"}
+dict_res = {}
 for gene, seq in dict_promoter.items():
     predict_v = model(seq2tensor(seq)).item() # predict_v 对应seq预测的活性值
+    dict_res[gene] = predict_v
     print(gene, predict_v) # 写到文件
+
+os.makedirs(f'Predictor/res/{task_id}', exist_ok=True)
+with open(f'Predictor/res/{task_id}/pred_v.csv', 'w', newline='') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(['gene', 'pred_v'])
+    for gene, pred_v in dict_res.items():
+        csv_writer.writerow([gene, pred_v])
+
+
+
--- grads/scores.py
+++ grads/scores.py
@@ -1,16 +1,75 @@
+# D:\code\git-project\Diff-Promoter\.venv\Scripts\python.exe D:\code\git-project\Diff-Promoter\grads\scores.py -task_id 1 -env NDM -species maize -gene gene_0:ACCTTGAAAGTATTTTTCACTGTATTTTGACGTCAGCCCATCACAATCTCGAAACCTTAAAGCTTATCGCGGCTTGCCCCGCCCACCACACGCACTGCCATGAATCCCCGCGCACTGATCATGCTCAGCACTGTCGTTTTCAGTGGGGGTGGCCAGAAAAGAGACCAGCT,gene_1:ACCTTGAAAGTATTTTTCACTGTATTTTGACGTCAGCCCATCACAATCTCGAAATTGGCCAGCTTATCGCGGCTTGCCCCGCCCACCACACGCACTGCCATGAATCCCCGCGCACTGATCATGCTCAGCATCGATGCTTTCAGTGGGGGTGGCCAGAAAAGAGACCAGCT
+import argparse
+import os
+
+import matplotlib
 import numpy as np
 import Bio.SeqIO
 import torch
 from captum.attr import DeepLiftShap
+from matplotlib import pyplot as plt
+
 from adv_model import Net
 from deeplift.dinuc_shuffle import dinuc_shuffle
 from deeplift.visualization import viz_sequence
 from itertools import islice
 
-device = torch.device('cuda:1')
+matplotlib.use('Agg')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-task_id', type=int, help='后端task_id')
+parser.add_argument('-env', type=str, help='使用环境')
+parser.add_argument('-species', type=str, help='maize | sorghum | arabidopsis')
+parser.add_argument('-gene', type=str,required=True, help='启动子序列，格式为"基因名:序列"，多个启动子用逗号分隔')
+
+args = parser.parse_args()
+task_id = args.task_id
+env = args.env
+
+device = None
+if hasattr(torch, 'cuda') and torch.cuda.is_available():
+    try:
+        device = torch.device('cuda:0')
+        # 简单测试CUDA是否真的可用
+        torch.zeros(1).to(device)
+    except:
+        device = None
+
+# if device is None and hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+#     device = torch.device('mps')
+
+if device is None:
+    device = torch.device('cpu')
+
+# 环境选择
+species = args.species
+file_path_species = ''
+if species == 'maize':
+    file_path_species = 'maize'
+elif species == 'sorghum':
+    file_path_species = 'sorghum'
+elif species == 'arabidopsis':
+    file_path_species = 'arabidopsis'
+else:
+    raise ValueError("species参数错误")
+file_path = f'Predictor-Guidance/{file_path_species}/'
+if env == 'WDM':
+    filename = file_path + "wdm/model_params.pkl"
+elif env == 'NDM':
+    filename = file_path + "ndm/model_params.pkl"
+elif env == 'NDT':
+    filename = file_path + "ndt/model_params.pkl"
+elif env == 'NLT':
+    filename = file_path + "nlt/model_params.pkl"
+elif env == 'WDT':
+    filename = file_path + "wdt/model_params.pkl"
+elif env == 'WLT':
+    filename = file_path + "wlt/model_params.pkl"
+else:
+    raise ValueError("env参数错误")
 
 model = Net()
-model.load_state_dict(torch.load('model_params.pkl')) # 3x6 18
+model.load_state_dict(torch.load(filename, map_location=device)) # 3x6 18
 model.to(device)
 model.eval()
 
@@ -35,28 +94,53 @@ def seqs2tensor(seqs, device=device):
     return torch.cat(seqs_tensor, 0)
 
 
-list_res = [] # 放任意条 170bp
-with open(r'../sswm_res/max_seq_change.csv', 'r') as f: # 换成序列dict
-    # for line in islice(f, 1, None):
-    for line in f:
-        list_res.append(line[:-1].split(','))
+# 解析启动子参数
+dict_promoter = {}
+if args.gene:
+    for item in args.gene.split(','):
+        gene, seq = item.split(':')
+        dict_promoter[gene] = seq.upper()
 
 
-seqs = np.array(list_res).T
+os.makedirs(f'grads/res/{task_id}', exist_ok=True)
+# 处理每个启动子序列
+for gene, seq in dict_promoter.items():
+    seq_tensor = seq2tensor(seq)
 
+    model.zero_grad()
+    dls = DeepLiftShap(model, multiply_by_inputs=False)
+    grads = dls.attribute(seq_tensor,
+                          baselines=seqs2tensor(dinuc_shuffle(seq, num_shufs=100, rng=np.random.RandomState(123))))
+    list_grads = (grads * seq_tensor).detach().cpu().squeeze(dim=0).numpy()
+    # viz_sequence.plot_weights_yxs(list_grads, subticks_frequency=20, savewhere=f'grads/res/{task_id}/' + gene + '.png')
+    viz_sequence.plot_weights(list_grads, subticks_frequency=20)
+    fig = plt.gcf()
+    outpath = f'grads/res/{task_id}/' + gene + '.png'
+    fig.savefig(outpath, bbox_inches='tight', dpi=300)
+    plt.close(fig)
 
-for res in seqs:
-    gene = res[0]
-
-    for iters, seq in enumerate(res[1:]):
-        seq = str(seq)
-        seq_tensor = seq2tensor(seq)
-
-        model.zero_grad()
-        dls = DeepLiftShap(model, multiply_by_inputs=False)
-        grads = dls.attribute(seq_tensor, baselines=seqs2tensor(dinuc_shuffle(seq, num_shufs=100, rng=np.random.RandomState(123))))
-        list_grads = (grads * seq_tensor).detach().cpu().squeeze(dim=0).numpy()
-        viz_sequence.plot_weights_yxs(list_grads, subticks_frequency=20, savewhere='./res/'+gene+'_iter_'+ str(iters) + '.png')
+# list_res = [] # 放任意条 170bp
+# with open(r'../sswm_res/max_seq_change.csv', 'r') as f: # 换成序列dict
+#     # for line in islice(f, 1, None):
+#     for line in f:
+#         list_res.append(line[:-1].split(','))
+#
+#
+# seqs = np.array(list_res).T
+#
+#
+# for res in seqs:
+#     gene = res[0]
+#
+#     for iters, seq in enumerate(res[1:]):
+#         seq = str(seq)
+#         seq_tensor = seq2tensor(seq)
+#
+#         model.zero_grad()
+#         dls = DeepLiftShap(model, multiply_by_inputs=False)
+#         grads = dls.attribute(seq_tensor, baselines=seqs2tensor(dinuc_shuffle(seq, num_shufs=100, rng=np.random.RandomState(123))))
+#         list_grads = (grads * seq_tensor).detach().cpu().squeeze(dim=0).numpy()
+#         viz_sequence.plot_weights_yxs(list_grads, subticks_frequency=20, savewhere='./res/'+gene+'_iter_'+ str(iters) + '.png')
 
 
 '''
